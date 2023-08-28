c017e9f4465abe39e9d71ab74dcd097325077e87
Phong Ly <lyquocphong@gmail.com>
2023-08-28 10:49:47 +0300

    Fix using linter

--- index.js
+++ index.js
@@ -1,160 +1,158 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const Person = require('./models/person')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Person = require('./models/person');
 
-const mongoose = require('mongoose')
+mongoose.set('strictQuery', false);
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGO_URI
-console.log('connecting to', url)
+const url = process.env.MONGO_URI;
+console.log('connecting to', url);
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
-
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message);
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
 
-    next(error)
-}
+  next(error);
+};
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.static('frontend'))
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static('frontend'));
 
 morgan.token('req-body', (req) => JSON.stringify(req.body));
-app.use(morgan(function (tokens, req, res) {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        tokens['req-body'](req, res)
-    ].join(' ')
-}));
+app.use(morgan((tokens, req, res) => [
+  tokens.method(req, res),
+  tokens.url(req, res),
+  tokens.status(req, res),
+  tokens.res(req, res, 'content-length'), '-',
+  tokens['response-time'](req, res), 'ms',
+  tokens['req-body'](req, res),
+].join(' ')));
 
 app.get('/api/persons', (request, response, next) => {
-    try {
-        Person.find({}).then(result => {
-            response.json(result)
-        })
-    } catch (error) {
-        next(error)
-    }
-})
+  try {
+    Person.find({}).then((result) => {
+      response.json(result);
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.post('/api/persons', async (request, response, next) => {
+  try {
+    const { name, number } = request.body;
 
-    try {
-        const { name, number } = request.body;
-
-        if (!name || name.length == 0 || !number || number.length == 0) {
-            return response.json({ error: 'name and number cannot be empty' }).status(400)
-        }
-
-        const exist = await Person.findById(request.params.id);
-
-        if (exist) {
-            return response.status(400).json({ error: 'name must by unique' });
-        }
-
-        const person = new Person({ name, number })
-        await person.save();
-        return response.status(201).end()
-    } catch (error) {
-        next(error)
+    if (!name || name.length == 0 || !number || number.length == 0) {
+      return response.json({ error: 'name and number cannot be empty' }).status(400);
     }
-})
+
+    const exist = await Person.findById(request.params.id);
+
+    if (exist) {
+      return response.status(400).json({ error: 'name must by unique' });
+    }
+
+    const person = new Person({ name, number });
+    await person.save();
+    return response.status(201).end();
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.put('/api/persons/:id', async (request, response, next) => {
-    try {
-        const { name, number } = request.body;
+  try {
+    const { name, number } = request.body;
 
-        if (!name || name.length == 0 || !number || number.length == 0) {
-            return response.json({ error: 'name and number cannot be empty' }).status(400)
-        }
-
-        const person = await Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true });
-
-        if (!person) {
-            return response.json({ error: 'not found' }).status(404);
-        }
-
-        return response.json(person)
-    } catch (err) {
-        next(err)
+    if (!name || name.length === 0 || !number || number.length === 0) {
+      return response.json({ error: 'name and number cannot be empty' }).status(400);
     }
-})
 
-app.get('/info', (request, response) => {
+    const person = await Person.findByIdAndUpdate(
+      request.params.id,
+      { name, number },
+      { new: true, runValidators: true, context: 'query' },
+    );
 
-    try {
-        Person.find({}).then(result => {
-            let html = `
+    if (!person) {
+      return response.json({ error: 'not found' }).status(404);
+    }
+
+    return response.json(person);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/info', (request, response, next) => {
+  try {
+    Person.find({}).then((result) => {
+      let html = `
     <p>Phonebook has info for ${result.length} people(s)</p>
     <br/>
     `;
 
-            html = html.concat((new Date()).toLocaleString());
+      html = html.concat((new Date()).toLocaleString());
 
-            response.send(html)
-        })
-    } catch (error) {
-        next(error)
-    }
-})
+      response.send(html);
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.get('/api/persons/:id', async (req, res, next) => {
+  try {
+    const person = await Person.findById(req.params.id);
 
-    try {
-        const person = await Person.findById(req.params.id);
-
-        if (person) {
-            return res.json(person);
-        }
-
-        return res.status(404).end();
-    } catch (err) {
-        next(err)
+    if (person) {
+      return res.json(person);
     }
-})
+
+    return res.status(404).end();
+  } catch (err) {
+    next(err);
+  }
+});
 
 app.delete('/api/persons/:id', async (req, res, next) => {
+  try {
+    const person = await Person.findByIdAndRemove(req.params.id);
 
-    try {
-        const person = await Person.findByIdAndRemove(req.params.id);
-
-        if (!person) {
-            return res.status(404).send('not found');
-        }
-
-        return res.status(200).json({ error: false })
-    } catch (err) {
-        next(err)
+    if (!person) {
+      return res.status(404).send('not found');
     }
-})
 
-app.use(unknownEndpoint)
-app.use(errorHandler)
+    return res.status(200).json({ error: false });
+  } catch (err) {
+    next(err);
+  }
+});
 
-const PORT = process.env.PORT || 3001
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
--- mongo.js
+++ mongo.js
@@ -1,58 +1,54 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument');
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-    `mongodb+srv://admin:${password}@study.evvb9xr.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://admin:${password}@study.evvb9xr.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-})
+  name: String,
+  number: String,
+});
 
-const Person = mongoose.model('Persons', personSchema)
+const Person = mongoose.model('Persons', personSchema);
 
 const getAll = () => {
-    Person.find({}).then(result => {
-        console.log('Phonebook:');
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
-    })
-}
+  Person.find({}).then((result) => {
+    console.log('Phonebook:');
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+};
 
 const addPerson = (name, number) => {
-    const person = new Person({ name, number })
+  const person = new Person({ name, number });
 
-    person.save().then(result => {
-        console.log(`Added ${name} number ${number} to phonebook`);
-        mongoose.connection.close()
-    })
-}
+  person.save().then(() => {
+    console.log(`Added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+};
 
 // Show all
-if (process.argv.length == 3) {
-    getAll()
-} else if (process.argv.length == 5) {
-    const name = process.argv[3];
-    const number = process.argv[4];
+if (process.argv.length === 3) {
+  getAll();
+} else if (process.argv.length === 5) {
+  const name = process.argv[3];
+  const number = process.argv[4];
 
-    if (name.length == 0 || number.length == 0) {
-        console.log('name and number cannot be empty')
-        process.exit(1)
-    }    
+  if (name.length === 0 || number.length === 0) {
+    console.log('name and number cannot be empty');
+    process.exit(1);
+  }
 
-    addPerson(name, number);
+  addPerson(name, number);
 }
-
-
-
